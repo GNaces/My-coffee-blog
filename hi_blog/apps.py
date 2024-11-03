e99b19688594dfbd11d037ba34f49cfdc6ae57b3
Godwin Naces <godwinnaces@gmail.com>
2024-11-03 05:14:46 +0000

    Tidying up, add doc string, validate code

--- hi_blog/apps.py
+++ hi_blog/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class HiBlogConfig(AppConfig):
+    """
+    Provides primary key type for blog app
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hi_blog'
